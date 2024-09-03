cf3f94e83c3fba4414ee759788a0216391da3a46
Muhammed-1123 <108229408+Muhammed-1123@users.noreply.github.com>
2024-09-03 18:34:01 +0300

    Calculator

--- index.js
+++ index.js
@@ -4,18 +4,40 @@ const Equal = document.querySelector(".equal");
 const Clear = document.querySelector(".Clear");
 const Delete = document.querySelector(".Delete");
 
+function calculate() {
+    try {
+        Result.textContent = eval(Result.textContent) || "0"; //
+    } catch (error) {
+        Result.textContent = "Error";
+    }
+}
+
+
 Buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
+        const value = e.target.textContent;
+        
         if (e.target.classList.contains("equal")) {
-            Result.textContent = eval(Result.textContent);
+            calculate();
         } else if (e.target.classList.contains("Clear")) {
             Result.textContent = "";
         } else if (e.target.classList.contains("Delete")) {
             Result.textContent = Result.textContent.slice(0, -1);
         } else {
-            Result.textContent += e.target.textContent;
-        }   
+            Result.textContent += value;
+        }
     });
 });
 
+document.addEventListener("keyup", (e) => {
+    if ((e.key >= '0' && e.key <= '9') || "+-*/.".includes(e.key)) {
+        Result.textContent += e.key;
+    } else if (e.key === "Enter") {
+        calculate();
+    } else if (e.key === "Backspace") {
+        Result.textContent = Result.textContent.slice(0, -1);
+    } else if (e.key === "Escape") {
 
+        Result.textContent = "";
+    }
+});
